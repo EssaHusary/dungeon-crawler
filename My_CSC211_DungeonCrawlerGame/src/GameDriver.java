2acdf94c66767a44b15e5270ccf438f27b987fec
Essa Husary <80508121+EssaHusary@users.noreply.github.com>
2025-03-16 15:22:53 -0700

    Update GameDriver.java

--- My_CSC211_DungeonCrawlerGame/src/GameDriver.java
+++ My_CSC211_DungeonCrawlerGame/src/GameDriver.java
@@ -1,5 +1,14 @@
 public class GameDriver {
     public static void main(String[] args){
+        Map map = new Map();
+        map.mapTraversal();  // To explore the map. This is a beta. Errors may occur as
+                             // things like pickups have yet to be integrated into the map.
 
+        CombatBeta beta = new CombatBeta();
+        beta.runCombat();  // To begin the combat loop on the final boss. This is also a beta.
+
+        /* The map traversal is slightly buggy as it hasn't been fully completed. The combat loop
+         runs decently well, so comment out line 4, the mapTraversal line to run the
+         combat loop. */
     }
 }
